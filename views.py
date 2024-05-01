f6ee2db0e63f6a87db0f3ca3ae01b2b968fd518d
Genevio <125595878+Genevio@users.noreply.github.com>
2024-05-01 08:46:34 +0400

    Add files via upload

---
+++ views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render ,redirect
+from .models import Task
+
+# Create your views here.
+def add(request):
+    if request.method == 'POST':
+        name = request.POST.get('name','')
+        priority = request.POST.get('priority','')
+
+        task=Task(name=name,priority=priority)
+        task.save()
+        return redirect('/')
+
+
+    return render(request,'myapp/add.html')
+
+def index(request):
+    task_list = Task.objects.all()
+    return render(request,'myapp/index.html',{'task_list':task_list})
